46e17d4e99132e044c4a6268882feb02b11cb169
Arnoud Bos <A.J.Bos@rn.rabobank.nl>
2016-10-16 14:07:33 +0200

    feat: component support fixes

--- src/fake-angular.js
+++ src/fake-angular.js
@@ -2,45 +2,47 @@
 
 var _ = require("lodash");
 
-var utils  = require("./utils");
-var api    = require("./api");
+var utils = require("./utils");
+var api = require("./api");
 var Module = require("./module");
 
-module.exports = function (options) {
+module.exports = function(options) {
 
-  options = _.merge({
-    hideAngularServices: false
-  }, options || {});
+    options = _.merge({
+        hideAngularServices: false
+    }, options || {});
 
-  var angular = {
-    modules: [],
-    modulesMap: {},
-    modulesNames: [],
-    options: options,
-    module: function(name, deps) {
-      var module;
+    var angular = {
+        modules: [],
+        modulesMap: {},
+        modulesNames: [],
+        options: options,
+        module: function(name, deps) {
+            var module;
 
-      // Module was inserted before
-      if (this.modulesNames.indexOf(name) !== -1) {
-        module = this.modulesMap[name];
-        if(deps){
-          this.modulesMap[name].modules = deps;
+            // Module was inserted before
+            if (this.modulesNames.indexOf(name) !== -1) {
+                module = this.modulesMap[name];
+                if (deps) {
+                    this.modulesMap[name].modules = deps;
+                }
+
+            // First time we see this module
+            } else {
+                module = new Module(name, deps, options);
+                this.modulesNames.push(name);
+                this.modulesMap[name] = module;
+                this.modules.push(module);
+            }
+
+            return module;
         }
-      // First time we see this module
-      } else {
-        module = new Module(name,deps, options);
-        this.modulesNames.push(name);
-        this.modulesMap[name] = module;
-        this.modules.push(module);
-      }
-      return module;
-    }
-  };
+    };
 
-  // Adds global apis to the angular object
-  api.globalApis.forEach(function(method) {
-    angular[method] = utils.noop;
-  });
+    // Adds global apis to the angular object
+    api.globalApis.forEach(function(method) {
+        angular[method] = utils.noop;
+    });
 
-  return angular;
+    return angular;
 };
--- src/module.js
+++ src/module.js
@@ -4,68 +4,79 @@ var _ = require("lodash");
 
 var pluralize = require("pluralize");
 var utils = require("./utils");
-var api   = require("./api");
+var api = require("./api");
 
 function Module(name, deps, options) {
-  this.name = name;
-  this.items = [];
-  this.controllers = [];
-  this.services = [];
-  this.factories = [];
-  this.filters = [];
-  this.providers = [];
-  this.directives = [];
-  this.components = [];
+    console.log("Module", name, deps, options);
+    this.name = name;
+    this.items = [];
+    this.controllers = [];
+    this.services = [];
+    this.factories = [];
+    this.filters = [];
+    this.providers = [];
+    this.directives = [];
+    this.components = [];
 
-  this.options = options;
+    this.options = options;
 
-  if (this.options.hideAngularServices) {
-    deps = _.filter(deps, function (dep) {
-      return !_.contains(api.angularServices, dep);
-    });
-  }
-  this.modules = deps;
+    // filter out angular dependencies
+    if (this.options.hideAngularServices) {
+        deps = _.filter(deps, function(dep) {
+            return !_.contains(api.angularServices, dep);
+        });
+    }
+
+    this.modules = deps;
 }
 
 // Adds module methods
 api.methods.forEach(function(method) {
-  Module.prototype[ method ] = function addItem(name) {
-    if (!name) {
-      return this;
-    }
-    this.items.push(name);
-    return this;
-  };
+    Module.prototype[method] = function addItem(name) {
+        if (!name) {
+            return this;
+        }
+        this.items.push(name);
+        return this;
+    };
 });
 
-["controller", "factory", "service", "filter", "provider", "directive", "component"].forEach(function (method) {
-  Module.prototype[ method ] = function (name, deps) {
-    if (!name) {
-      return this;
-    }
-    deps = utils.parseAngularDeps(deps).deps;
+["controller", "factory", "service", "filter", "provider", "directive", "component"].forEach(function(method) {
+    Module.prototype[method] = function(name, deps) {
+        if (!name) {
+            return this;
+        }
 
-    // Exclude angular services from dependencies
-    if (this.options.hideAngularServices) {
-      deps = _.filter(deps, function (dep) {
-        return !_.contains(api.angularServices, dep);
-      });
-    }
-    this[pluralize(method)].push({
-      "name": name,
-      "deps": deps
-    });
-    this.items.push(name);
-    return this;
-  };
+        if (method === "component" && deps.controller && typeof deps.controller === "function") {
+            deps = deps.controller;
+        }
+
+        deps = utils.parseAngularDeps(deps).deps;
+
+        // Exclude angular services from dependencies
+        if (this.options.hideAngularServices) {
+            deps = _.filter(deps, function(dep) {
+                return !_.contains(api.angularServices, dep);
+            });
+        }
+
+        this[pluralize(method)].push({
+            "name": name,
+            "deps": deps
+        });
+
+        this.items.push(name);
+
+        return this;
+    };
 });
 
 Module.prototype.run = function() {
-  return this;
+    return this;
 };
 
 Module.prototype.config = function() {
-  return this;
+    return this;
 };
 
 module.exports = Module;
