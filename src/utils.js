cd44a70816f0593f1dfd45f76409aca109e3703f
Arnoud Bos <A.J.Bos@rn.rabobank.nl>
2016-11-04 13:39:56 +0100

    fix

--- src/utils.js
+++ src/utils.js
@@ -31,8 +31,32 @@ function parseAngularDeps(angularDeps) {
         }
     }
 
+    // check if provider and scoop up the nested dependencies of the service which has probably many more deps
+    // that the provider has.
+    var re = /\$get\s*\:\s*([^\s]*)/m;
+    var matches = re.exec(angularDeps.toString());
+    var deps2 = [];
+
+    if (matches && matches.length > 0) {
+        var nestedServiceName = matches[1];
+        var re2Src = "\\s+" + nestedServiceName + "\\.\\$inject\\s*=\\s*\\[([^\\]]*)\\]";
+        var re2 = new RegExp(re2Src, "m");
+        var matches2 = re2.exec(angularDeps.toString());
+
+        if (matches2 && matches2.length > 0) {
+            deps2 = matches2[1].replace(/["']/g, '').split(/\s*,\s*/);
+        }
+    }
+
     if (deps && deps.length) {
         deps.forEach(function(dep) {
+            dep = dep.trim();
+            depsProcessed.push(dep);
+        });
+    }
+
+    if (deps2 && deps2.length) {
+        deps2.forEach(function(dep) {
             dep = dep.trim();
             depsProcessed.push(dep);
         });
